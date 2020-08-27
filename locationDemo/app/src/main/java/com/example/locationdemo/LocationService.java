983d7a809a9666e68312fee995d8d43c3e202ff5
Sarapata <asarapata@macbook-pro-de-anthony.home>
2020-08-27 15:11:36 +0200

    LocationService in pro - permissions ok

---
+++ locationDemo/app/src/main/java/com/example/locationdemo/LocationService.java
@@ -0,0 +1,51 @@
+package com.example.locationdemo;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.os.Looper;
+import android.util.Log;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+
+
+public class LocationService extends Service {
+    FusedLocationProviderClient fusedLocationProviderClient;
+    LocationCallback locationCallback;
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        locationCallback = new LocationCallback(){
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+                Log.d("my car", "Latitude is" + locationResult.getLastLocation().getLatitude()
+                        + "," + "Longitude is" + locationResult.getLastLocation().getLongitude());
+            }
+        };
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        requestLocation();
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    private void requestLocation() {
+        LocationRequest locationRequest = new LocationRequest();
+        locationRequest.setInterval(5000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+        fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, Looper.myLooper());
+    }
+}
